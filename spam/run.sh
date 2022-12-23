f1c2cd6f30788138ca937352c8a2beffce136e6a
drbean <drbean@freeshell.org>
2022-12-23 13:33:33 +0800

    newline_between_spam_runs

--- spam/run.sh
+++ spam/run.sh
@@ -2,6 +2,7 @@
 
 x=$(eval echo {$1..$2})
 for i in ${x[@]} ; do
+	echo -e "\n" >> $HOME/job/done
 	echo north: $i of $x
 	$HOME/job/spam.sh < $HOME/job/north.$i
 	echo $(date) north.$i >> $HOME/job/done
