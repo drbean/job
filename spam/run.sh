a18eef0dc6d4b029819679813e5cf6c85ba30fdd
drbean <drbean@freeshell.org>
2022-12-24 20:23:59 +0800

    newline_before_files_done.BSD.echo:no_e_flag

--- spam/run.sh
+++ spam/run.sh
@@ -1,10 +1,10 @@
 #!/usr/pkg/bin/bash
 
+echo >> $HOME/job/done
 x=$(eval echo {$1..$2})
 for i in ${x[@]} ; do
 	echo north: $i of $x
 	$HOME/job/spam.sh < $HOME/job/north.$i
 	echo $(date) north.$i >> $HOME/job/done
 done
-echo -e "\n" >> $HOME/job/done
 
