0ecc625cd1252b46b2ca8703f9f2b0f92ff9ddc8
drbean <drbean@freeshell.org>
2022-12-28 07:03:20 +0800

    first~last: help find place in case of killed run

--- spam/run.sh
+++ spam/run.sh
@@ -3,8 +3,11 @@
 echo >> $HOME/job/done
 x=$(eval echo {$1..$2})
 for i in ${x[@]} ; do
+	declare -i first last
+	first=$(< next)
 	echo north: $i of $x
 	$HOME/job/spam.sh < $HOME/job/north.$i
-	echo $(date) north.$i >> $HOME/job/done
+	last=$(< next)
+	echo $(date) $first~$last north.$i >> $HOME/job/done
 done
 
