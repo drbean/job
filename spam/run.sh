b80f86e400f9ee99466673c0e5062443736565d2
drbean <drbean@freeshell.org>
2023-04-26 22:02:10 +0900

    besides AREA arg, if 1 other arg, 0xx0..0xx9, if 2 other, x..y

--- spam/run.sh
+++ spam/run.sh
@@ -3,7 +3,18 @@
 area=$1
 
 echo >> $HOME/job/$area/done
-x=$(eval echo {$2..$3})
+if [[ $# -eq 2 ]]; then
+	x=$(eval echo {0${2}0..0${2}9})
+elif [[ $# -eq 3 ]]; then
+	if [[ !($2 -le $3) ]]; then
+		echo "$@ backwards!" >&2
+		exit
+	fi
+	x=$(eval echo {$2..$3})
+else
+	echo "$@: Too many/few args?" >&2
+	exit
+fi
 for i in ${x[@]} ; do
 	declare -i first last
 	first=$(< $HOME/job/$area/next)
@@ -13,4 +24,3 @@ for i in ${x[@]} ; do
 	echo $(date) $((++first))~$last $area.$i |
 		tee -a $HOME/job/$area/done
 done
-
