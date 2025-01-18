3b9e1e3490ad5a945948239ad0461713f9cf2126
Dr Bean <drbean@freeshell.org>
2025-01-18 21:28:39 +0900

    s/ SKELETON / MEAT / , script_nameref, yq_nameref=component for greeting, intro, promotion, strength.category by read here_string

--- mail/bone.sh
+++ mail/bone.sh
@@ -2,7 +2,7 @@
 
 #!/usr/pkg/bin/bash
 
-SKELETON=$HOME/edit/email/edit_offer/skeleton.yaml
+MEAT=$HOME/edit/email/edit_offer/skeleton.yaml
 declare -A N PICK language care understanding experience roman alpha num
 
 component=(greeting intro promotion strength point separator weakness pitch toeflic outro)
@@ -10,12 +10,12 @@ strength=(language care understanding experience)
 point=(roman alpha num) 
 
 for c in ${component[*]} ; do
-	N[$c]=$(yq ".$c | length" $SKELETON)
+	N[$c]=$(yq ".$c | length" $MEAT)
 	echo .$c length: ${N[$c]}
 done
 
 for s in ${strength[*]} ; do
-	N[$s]=$(yq ".strength.$s | length" $SKELETON) 
+	N[$s]=$(yq ".strength.$s | length" $MEAT) 
 	echo .strength.$s length: ${N[$s]}
 done
 
@@ -25,21 +25,38 @@ for p in ${component[*]} ${strength[*]} ; do
 done
 
 for c in ${component[*]} ; do
+	declare -n script_nameref=$c
 	if [[ -v $c ]] ; then
-		declare -n yq_nameref=$c
-		for s in ${yq_nameref[*]} ; do
-			echo $c.$s: $(yq ".$c.$s.[${PICK[$s]}]" $SKELETON)
-		done
-		else echo $c: $(yq ".$c.[${PICK[$c]}]" $SKELETON)
+		if [[ $c == "strength" ]] ; then
+			declare -n yq_nameref=$c
+			declare -a category
+			declare -i i=0
+			while read p ; do
+				category[$i]=$p
+				echo ${category[$i]}
+				i+=1
+			done <<<$(yq ".strength | keys.[]" $MEAT)
+			for s in ${category[*]} ; do
+				n=$(yq ".$c.$s | length" $MEAT)
+				r=$(( $RANDOM % $n ))
+				echo $s: $r
+				script_nameref=$(yq ".$c.$s.$r" $MEAT)
+			done
+		fi
+	else 
+		n=$(yq ".$c | length" $MEAT)
+		r=$(( $RANDOM % $n ))
+		script_nameref=$(yq ".$c.[$r]" $MEAT)
 	fi
 done
 
 cat <<OFFER
-$(yq ".greeting.[${PICK[greeting]}]" $SKELETON)!
+$greeting!
 
-$(yq ".intro.[${PICK[intro]}]" $SKELETON).
+$intro.
 
-$(yq ".promotion.[${PICK[promotion]}]" $SKELETON):
+$promotion:
 
-$(yq ".point.roman.[0]" $SKELETON)$(yq ".separator.[${PICK[separator]}]" $SKELETON)
+$(yq ".point.roman.[0]" $MEAT)$(yq ".separator.[${PICK[separator]}]" $MEAT)
+$strength
 OFFER
