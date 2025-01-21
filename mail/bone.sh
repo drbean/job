6a88a1bee34b63698c1e8ea404d4a05a2273771f
Dr Bean <drbean@freeshell.org>
2025-01-21 20:38:56 +0900

    yq .strength | keys | shuffle, order readarray, order_nameref=strengthi, strength_nameref, pointi: remove matching suffix unnecessary with .[]. plus weakness, pitch, toeflic, outro

--- mail/bone.sh
+++ mail/bone.sh
@@ -14,14 +14,23 @@ for c in ${component[*]} ; do
 	if [[ -v $c ]] ; then
 		if [[ $c == "strength" ]] ; then
 			declare -n strength_nameref=$c
-			key_n=$(yq ".strength | keys | length" $MEAT)
-			key_index=$(( $RANDOM % $key_n ))
-			script_nameref=${script_nameref[$key_index]}
-			echo key index: $key_index, script_nameref: $script_nameref
-			array_n=$(yq ".strength.$script_nameref | length" $MEAT)
-			array_index=$(( $RANDOM % $array_n ))
-			strength_nameref=$(yq ".strength.$strength_nameref.[$array_index]" $MEAT)
-			echo array index: $array_index, strength_nameref: $strength_nameref
+			declare -a order
+			readarray -t order <<<$(yq ".strength | keys | shuffle | .[]" $MEAT)
+			echo order array: ${order[@]}
+			declare -a strength
+			for i in ${!order[@]} ; do
+				echo order: $i
+				key=${order[$i]}
+				key_n=$(yq ".strength.$key | length" $MEAT)
+				key_index=$(( $RANDOM % $key_n ))
+				strength_nameref=$key
+				array_n=$(yq ".strength.$key | length" $MEAT)
+				array_index=$(( $RANDOM % $array_n ))
+				strength_nameref=$(yq ".strength.$strength_nameref.[$array_index]" $MEAT)
+				echo array index: $array_index, strength_nameref: $strength_nameref
+				declare -n order_nameref=strength$((i+=1)); order_nameref=$strength_nameref
+			done
+			echo strength1: $strength1, strength2: $strength2, strength3: $strength3, strength4: $strength4
 		elif [[ $c == "point" ]] ; then
 			# for i in {0..3} ; do
 			# 	declare -n point${i}_nameref=$point$i
@@ -31,11 +40,11 @@ for c in ${component[*]} ; do
 			script_nameref=${script_nameref[$key_index]}
 			echo key index: $key_index, script_nameref: $script_nameref
 			declare -a point
-			readarray -t point <<<$(yq ".point.$script_nameref" $MEAT)
-				point1=${point[0]#- }
-				point2=${point[1]#- }
-				point3=${point[2]#- }
-				point4=${point[3]#- }
+			readarray -t point <<<$(yq ".point.$script_nameref | .[]" $MEAT)
+				point1=${point[0]}
+				point2=${point[1]}
+				point3=${point[2]}
+				point4=${point[3]}
 				echo point1: $point1, point2: $point2, point3: $point3, point4: $point4
 			echo point_array: ${point[@]}
 		fi
@@ -53,5 +62,16 @@ $intro.
 
 $promotion:
 
-$point1$separator $strength, $point2$separator $strength, $point3$separator $strength, $point4$separator $strength
+$point1$separator $strength1,
+$point2$separator $strength2,
+$point3$separator $strength3,
+$point4$separator $strength4
+
+A weakness is: $weakness
+
+$pitch
+
+$toeflic
+
+$outro
 OFFER
